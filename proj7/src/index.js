a68d747819b76dd91bcd22fe120833031dda24c6
Bruno <brunohgrau99@gmail.com>
2024-02-03 22:39:13 -0300

    Commit

---
+++ proj7/src/index.js
@@ -0,0 +1,1 @@
+module.exports.asynchronousRead = require("./readfile/asynchronous.read").read;
---
+++ proj7/src/write/asynchronous.write.js
@@ -0,0 +1,5 @@
+const { writeFile } = require("fs");
+
+writeFile("./data/app.log", "Tampa Tribune,238877,191477,0,0", (err) => {
+  err ? console.log(err) : console.log("file saved");
+});
