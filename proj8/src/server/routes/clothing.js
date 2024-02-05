9e240ee99506b210e1cc1d46ede56336145f1dba
Bruno <brunohgrau99@gmail.com>
2024-02-05 08:42:51 -0300

    Commit Proj8

--- proj8/src/server/routes/clothing.js
+++ proj8/src/server/routes/clothing.js
@@ -1,20 +1,47 @@
 const express = require("express");
 const fs = require("fs");
+const fsPromises = require("fs").promises;
 const datafile = "server/data/clothing.json";
 const router = express.Router();
 
 /* GET all clothing */
-router.route("/").get(function (req, res) {
-  fs.readFile(datafile, "utf-8", (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      let clothingData = JSON.parse(data);
-      console.log("Returning clothing data");
-      res.send(clothingData);
-    }
-  });
-  console.log("Doing more work");
+router.route("/").get(async function (req, res) {
+  try {
+    let data = await getClothingData();
+    console.log("Returning async data.");
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+
+  // getClothingData()
+  //   .then(data => {
+  //     console.log('Returning clothing data to browser.');
+  //     res.send(data);
+  //   })
+  //   .catch(error => res.status(500).send(error))
+  //   .finally(() => console.log('All done processing promise.'));
 });
+
+async function getClothingData() {
+  let rawData = await fsPromises.readFile(datafile, "utf8");
+  let clothingData = JSON.parse(rawData);
+
+  console.log(clothingData);
+
+  return clothingData;
+
+  // return new Promise((resolve, reject) => {
+  //   fs.readFile(datafile, 'utf8', (err, data) => {
+  //     if (err) {
+  //       reject(err);
+  //     }
+  //     else {
+  //       let clothingData = JSON.parse(data);
+  //       resolve(clothingData);
+  //     }
+  //   });
+  // });
+}
 
 module.exports = router;
