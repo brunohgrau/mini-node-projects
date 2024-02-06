9b2a496227d1136d0cafa149cf09511debe53d8f
Bruno <brunohgrau99@gmail.com>
2024-02-06 13:36:24 -0300

    Server

---
+++ restaurant-web-server/src/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import workingHours from "./data/workingHours.js";
+import menuItems from "./data/menuItems.js";
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { name: "Whats fair?" });
+});
+
+app.get("/menu", (req, res) => {
+  res.render("menu", { menuItems });
+});
+app.get("/hours", (req, res) => {
+  const days = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+  ];
+  res.render("hours", { workingHours, days });
+});
+
+app.listen(port, () => {
+  console.log(`Web Server is listening at localhost:${port}`);
+});
